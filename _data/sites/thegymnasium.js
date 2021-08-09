c3aa4cf23afd70effcf827bf9e19f268b81582c8
Roman Edirisinghe <rediris@users.noreply.github.com>
2021-08-09 15:38:42 -0400

    Add gymnasium

---
+++ _data/sites/thegymnasium.js
@@ -0,0 +1,27 @@
+module.exports = {
+	name: "thegymnasium.com", // optional, falls back to object key
+	description: "Gymnasium",
+	// skip if localhost
+	// skip if this is a new fork of the speedlify (not Zach’s)
+	skip: false,
+	options: {
+		frequency: 60 * 6, // 6 hours
+		// Use "run" if the sites don’t share assets on the same origin
+		//           and we can reset chrome with each run instead of
+		//           each site in every run (it’s faster)
+		// Use "site" if sites are all on the same origin and share assets.
+		freshChrome: "run"
+	},
+	urls: [
+		"https://thegymnasium.com/",
+		"https://thegymnasium.com/about",
+		"https://thegymnasium.com/accessibility",
+		"https://thegymnasium.com/collections",
+		"https://thegymnasium.com/courses",
+		"https://thegymnasium.com/design-systems",
+		"https://thegymnasium.com/jobs",
+		"https://thegymnasium.com/webinars",
+		"https://thegymnasium.com/web-development",
+		"https://thegymnasium.com/ux-design",
+	]
+};
