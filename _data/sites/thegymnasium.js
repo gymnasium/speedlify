3865202e846fbb4346c96cf63f5e33f3fd7ef2bb
Roman Edirisinghe <rediris@users.noreply.github.com>
2021-08-11 15:10:34 -0400

    Update frequency, remove URLs to help reduce build time

--- _data/sites/thegymnasium.js
+++ _data/sites/thegymnasium.js
@@ -1,11 +1,10 @@
 module.exports = {
 	name: "thegymnasium.com", // optional, falls back to object key
 	description: "Gymnasium",
-	// skip if localhost
-	// skip if this is a new fork of the speedlify (not Zach’s)
 	skip: false,
 	options: {
-		frequency: 60 * 6, // 6 hours
+    // to set minimum time needed before next measurement
+		frequency: 60 * 1, // minimum of once per 60 minutes
 		// Use "run" if the sites don’t share assets on the same origin
 		//           and we can reset chrome with each run instead of
 		//           each site in every run (it’s faster)
@@ -16,11 +15,8 @@ module.exports = {
 		"https://thegymnasium.com/",
 		"https://thegymnasium.com/about",
 		"https://thegymnasium.com/accessibility",
-		"https://thegymnasium.com/collections",
 		"https://thegymnasium.com/courses",
-		"https://thegymnasium.com/design-systems",
 		"https://thegymnasium.com/jobs",
-		"https://thegymnasium.com/webinars",
 		// Add one of each of our most popular courses by type
 		"https://thegymnasium.com/courses/GYM/103/0/about", // Full: UX Fundamentals
 		"https://thegymnasium.com/courses/course-v1:GYM+016+0/about", // Gym Short: Designing Responsible & Accessible Websites
