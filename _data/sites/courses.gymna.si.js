f103d8b823cd4d13db2ee2de55dce819c0c7b2f4
Roman Edirisinghe <rediris@users.noreply.github.com>
2021-08-18 14:48:43 -0400

    Add courses.gymna.si

---
+++ _data/sites/courses.gymna.si.js
@@ -0,0 +1,25 @@
+module.exports = {
+	name: "courses.gymna.si", // optional, falls back to object key
+	description: "gymna.si",
+	skip: false,
+	options: {
+    // to set minimum time needed before next measurement
+		frequency: 60 * 7, // Max of once every 7 hours (see the github action for the actual CRON run time)
+		// Use "run" if the sites don’t share assets on the same origin
+		//           and we can reset chrome with each run instead of
+		//           each site in every run (it’s faster)
+		// Use "site" if sites are all on the same origin and share assets.
+		freshChrome: "run"
+	},
+	urls: [
+		"https://courses.gymna.si/",
+		"https://courses.gymna.si/about",
+		"https://courses.gymna.si/accessibility",
+		"https://courses.gymna.si/courses",
+		"https://courses.gymna.si/jobs",
+		// Add one of each of our most popular courses by type
+		"https://courses.gymna.si/courses/GYM/103/0/about", // Full: UX Fundamentals
+		"https://courses.gymna.si/courses/course-v1:GYM+016+0/about", // Gym Short: Designing Responsible & Accessible Websites
+		"https://courses.gymna.si/courses/take5/adding-a-css-gradient-overlay-to-an-image", // Take 5
+	]
+};
